0fa11fe5ddc162ab495fbbe18f204b8dae7bfae7
Nick Chitwood <nicholaschitwood@gmail.com>
2019-08-27 16:59:58 -0700

    Vastly improved functionality, including initial shiny visualization

--- CSI-ATSI/app.R
+++ CSI-ATSI/app.R
@@ -8,42 +8,287 @@
 #
 
 library(shiny)
+library(DT)
+
+support_level <- readRDS('support_level.RDS')
+group_file <- readRDS('group_file.RDS')
+school_choices <- sort(support_level$school_w_district)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
+    # Border style
+    tags$style(HTML("
+      #red-border {
+          border: 4px solid red;
+      }
+    ")),
+    
     # Application title
-    titlePanel("CSI-ATSI Report"),
+    titlePanel("CSI-ATSI School Report - 2018-2019 Identification Cycle"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectizeInput("schoolName",
+                        "Please type in a school name:",
+                        choices = c("Choose"="", school_choices),
+                        width = "400px",
+                        selected = ""),
+            fluidRow(
+                div("This application enables you to lookup any school in the State of California and better understand its status within the California System of Support. ")
+            )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           fluidRow(
+                      div(h3(strong("School:"), textOutput("title", inline = TRUE)), p(em("CDS: "),textOutput("cds", inline = TRUE)))
+           ),
+           hr(),
+           fluidRow(h3("School Demographics")),
+           fluidRow(
+               column(3,
+                      div(em("Total Enrollment"), 
+                          textOutput("totalenrollment"))
+                      ),
+               column(2,
+                      div(em("EL%"), 
+                          textOutput("el"))
+                      ),
+               column(2,
+                      div(em("SED%"), 
+                          textOutput("sed"))
+                      ),
+               column(2,
+                      div(em("SWD%"), 
+                          textOutput("swd"))
+                      ),
+               column(2,
+                      div(em("FOS%"), 
+                          textOutput("fos"))
+                      )
+           ),
+           hr(),
+           fluidRow(h3("Identification Status")),
+           fluidRow(h4(textOutput("status"))),
+           hr(),
+           fluidRow(h3("CSI - Grad Rate Test")),
+           fluidRow(p("The first test for Comprehensive Support is for the school's grad rate. A grad rate of below 67% automatically qualifies as school for CSI.")),
+           fluidRow(column(2,
+                           div(em("Average Grad Rate (%):"), textOutput("gradrate"))
+                           ),
+                    column(2,
+                           div(em("Grad rate below 67%?"), 
+                            strong(textOutput("csi_grad"))
+                           )
+                    )
+           ),
+           hr(),
+           fluidRow(h3("CSI - Low Performing")),
+           fluidRow(p("The next test for Comprehensive Support is for the school's outcomes on the CA School Dashboard. There are several combinations of dashboard colors which can results in identification as CSI, Low Performing.")),
+           fluidRow(h4("Dashboard Data")),
+           dataTableOutput("dashboard"),
+           fluidRow(h4("CSI Tests")),
+           fluidRow(
+               column(3,
+                      div(em("All Red"), p("Are all dashboard indicators red?"), 
+                          strong(textOutput("all_red")))
+               ),
+               column(3,
+                      div(em("All red but 1"), p("Are all dashboard indicators red but one?"), 
+                          strong(textOutput("all_red_but_1")))
+               ),
+               column(3,
+                      div(em("Majority red"), p("Are the majority of dashboard indicators red when there are at least 5 indicators?"), 
+                          strong(textOutput("majority_red")))
+               ),
+               column(3,
+                      div(em("Red and orange"), p("Are all of the indicators red and orange?"),
+                          strong(textOutput("red_and_orange")))
+               )
+           ),
+           hr(),
+           fluidRow(h3("ATSI Data")),
+           fluidRow(h4("Student Group Results on Past Two Dashboards")),
+           fluidRow(h5("Socioeconomically Disadvantaged")),
+           dataTableOutput("groups_sed")
+           
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    schoolData <- reactive({
+        schoolData <- support_level %>%
+            filter(school_w_district == input$schoolName) %>%
+            mutate(grad_average = round(grad_average, digits = 1))
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        return(schoolData)    
+        })
+    
+    dashboard <- reactive({
+        req(input$schoolName)
+        dashboard <- support_level %>%
+            filter(school_w_district == input$schoolName) %>%
+            select(`CCI` = cci,
+                   `Chronic Absenteeism` = chronic, 
+                   `Suspension Rate` = susp,
+                   `Grad Rate` = grad, 
+                   `ELA` = ela, 
+                   `Mathematics` = math 
+    )
+        return(dashboard)
     })
-}
+    
+    cds <- reactive({
+        schoolData() %>%
+            pull(cds)
+    })
+    
+    groups <- reactive({
+        req(cds())
+        school_groups <- group_file %>%
+            filter(cds == cds()) %>%
+            select(`Student Group` = studentgroup,
+                   `Year`= year,
+                   `CCI` = cci,
+                   `Chronic Absenteeism` = chronic, 
+                   `Suspension Rate` = susp,
+                   `Grad Rate` = grad, 
+                   `ELA` = ela, 
+                   `Mathematics` = math 
+            )
+    
+        studentgrouplist <- c(
+            'SED',
+            'EL',
+            'FOS',
+            'SWD',
+            'HOM',
+            'AA',
+            'AI',
+            'AS',
+            'FI',
+            'HI',
+            'MR',
+            'WH'
+        )
+
+        get_group_data <- function(group,dataframe) {
+            results <- dataframe %>%
+                filter(`Student Group` == group)
+            return(results)
+        }
+        
+        results <- lapply(studentgrouplist, get_group_data, dataframe = school_groups)
+    })
+    
+    output$title <-
+        renderText({
+            input$schoolName
+            })
+
+    output$cds <-
+        renderText({
+            schoolData()$cds
+        })
+    
+    output$el <-
+        renderText({
+            schoolData()$EL
+        })
+    output$fos <-
+        renderText({
+            schoolData()$FOS
+        })
+    output$sed <-
+        renderText({
+            schoolData()$SED
+        })
+    output$swd <-
+        renderText({
+            schoolData()$SWD
+        })
+    output$totalenrollment <-
+        renderText({
+            schoolData()$totalenrollment
+        })
+    output$status <-
+        renderText({
+            schoolData()$identification
+        })
+    output$dashboard <-
+        renderDT({
+            datatable(
+                dashboard(), 
+                options = list(
+                    dom = 't',
+                    searching = FALSE,
+                    autoWidth = TRUE,
+                    columnDefs = list(list(width = '16.6%', targets = c(1, 2, 3, 4, 5, 6 )))
+                    )
+                ) %>% 
+                formatStyle(names(dashboard()), 
+                              backgroundColor = styleEqual(
+                                  c("Red","Orange","Yellow","Green","Blue"),
+                                  c('rgba(255,0,0,0.5)','rgba(255,165,0,0.5)','rgba(255,255,0,0.5)','rgba(0,255,0,0.5)','rgba(0,0,255,0.5)')
+                              )
+                     )
+        }) 
+
+    output$groups_sed <-
+        renderDT({
+            datatable(
+                groups()[[1]],
+                options = list(
+                    dom = 't',
+                    searching = FALSE,
+                    autoWidth = TRUE,
+                    columnDefs = list(list(width = '8%', targets = c(1, 2)),
+                                      list(width = '14%', targets = c(3, 4, 5, 6, 7, 8 ))
+                                      )
+                )
+            ) %>% 
+                formatStyle(names(groups()[[1]]), 
+                            backgroundColor = styleEqual(
+                                c("Red","Orange","Yellow","Green","Blue"),
+                                c('rgba(255,0,0,0.5)','rgba(255,165,0,0.5)','rgba(255,255,0,0.5)','rgba(0,255,0,0.5)','rgba(0,0,255,0.5)')
+                            )
+                )
+        })
+
+    output$gradrate <-
+        renderText({
+            schoolData()$grad_average
+        })
+    output$csi_grad <-
+        renderText({
+            schoolData()$csi_grad
+        })
+    output$all_red <-
+        renderText({
+            schoolData()$all_red
+        })
+    output$all_red_but_1 <-
+        renderText({
+            schoolData()$all_red_but_1
+        })
+    output$majority_red <-
+        renderText({
+            schoolData()$majority_red
+        })
+    output$red_and_orange <-
+        renderText({
+            schoolData()$red_and_orange
+        })
+
+    
+    
+    }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- prepare_data.R
+++ prepare_data.R
@@ -1,6 +1,87 @@
 library(tidyverse)
 
-# Import 2018 data
+# Import 2017 data
+
+eladownload2017 <- read_tsv('data/2017/ela38download2017f.txt') %>%
+  filter(
+    rtype == "S"
+  ) %>%
+  select(cds,
+         studentgroup,
+         color) %>%
+  mutate(
+    indicator = "ela",
+    year = "2017"
+  )
+
+elpidownload2017 <- read_tsv('data/2017/elpidownload2017f.txt') %>%
+  filter(
+    rtype == "S"
+  ) %>%
+  select(cds,
+         color) %>%
+  mutate(
+    studentgroup = "EL",
+    indicator = "elpi",
+    year = "2017"
+  )
+
+
+graddownload2017 <- read_tsv('data/2017/graddownload2017f.txt') %>%
+  filter(
+    rtype == "S"
+  ) %>%
+  select(cds,
+         studentgroup,
+         color) %>%
+  mutate(
+    indicator = "grad",
+    year = "2017"
+  )
+
+mathdownload2017 <- read_tsv('data/2017/math38download2017f.txt') %>%
+  filter(
+    rtype == "S"
+  ) %>%
+  select(cds,
+         studentgroup,
+         color) %>%
+  mutate(
+    indicator = "math",
+    year = "2017"
+  )
+
+suspdownload2017 <- read_tsv('data/2017/suspdownload2017f.txt') %>%
+  filter(
+    rtype == "S"
+  ) %>%
+  select(cds,
+         studentgroup,
+         color) %>%
+  mutate(
+    indicator = "susp",
+    year = "2017"
+  )
+
+all2017 <- bind_rows(
+  eladownload2017, 
+  elpidownload2017,
+  graddownload2017,
+  mathdownload2017, 
+  suspdownload2017
+)
+
+rm(list = c(
+          "eladownload2017", 
+          "elpidownload2017",
+          "graddownload2017",
+          "mathdownload2017", 
+          "suspdownload2017"
+          )
+  )
+
+
+# Import 2018 Dashboard Data
 
 cci2018 <- read_tsv('data/2018/ccidownload2018.txt') %>%
   filter(
@@ -56,10 +137,18 @@ gradrate <- read_tsv('data/2018/graddownload2018.txt') %>%
     studentgroup == "ALL"
   ) %>%
   mutate(
-    grad_average = (currstatus + priorstatus) / 2,
+    total_num = (currnumer + priornumer),
+    total_den = (currdenom + priordenom),
+    grad_average = total_num/total_den * 100,
     indicator = "grad_avg",
-    year = "2018"
+    year = "2018",
+    usable_rate = currdenom >= 30 & priordenom >= 30
   ) %>% 
+  mutate(grad_average = if_else(
+    usable_rate == TRUE, 
+    grad_average, 
+    NULL)
+         ) %>%
   select(
     cds,
     studentgroup,
@@ -102,25 +191,24 @@ all2018 <- bind_rows(
 )
 
 rm(list = c(
-          "cci2018", 
-          "chronicdownload2018", 
-          "eladownload2018", 
-          "graddownload2018",
-          "mathdownload2018", 
-          "suspdownload2018"
-          )
-  )
+  "cci2018", 
+  "chronicdownload2018", 
+  "eladownload2018", 
+  "graddownload2018",
+  "mathdownload2018", 
+  "suspdownload2018"
+)
+)
 
-# Import state data
-state_elig <- read_csv('data/sch_eligibility_state_sg.csv') %>%
-  select(cds = CDS,
-         TitleI1718,
-         CDEStatus = AssistanceStatus)
+# Combine years
+all_years <- bind_rows(
+  all2018,
+  all2017
+)
 
-
-# Determine eligibility - One row per school per student group
-all2018_wide_test <- all2018 %>%
-  group_by(cds, studentgroup) %>%
+# Determine CSI eligibility - One row per school per student group
+all_wide_test <- all_years %>%
+  group_by(year, cds, studentgroup) %>%
   summarize(
     red_count = sum(color == 1, na.rm = TRUE),
     orange_count = sum(color ==2, na.rm = TRUE),
@@ -129,15 +217,30 @@ all2018_wide_test <- all2018 %>%
     ) %>%
   ungroup() %>%
   mutate(
-    all_red = if_else((red_count == all_count) & (all_count > 0), TRUE, FALSE),
+    all_red = if_else(studentgroup == "ALL",
+              if_else(
+                (red_count == all_count) & (all_count > 0), 
+                TRUE, 
+                FALSE
+                ),
+              if_else(
+                (red_count == all_count) & (all_count > 1), 
+                TRUE, 
+                FALSE
+                )
+              ),
     all_red_but_1 = if_else((red_count == (all_count - 1)) & (all_count > 0) & (red_count > 0), TRUE, FALSE),
     majority_red = if_else((as.numeric(red_count) > (all_count / 2)) & all_count >= 5, TRUE, FALSE),
-    red_and_orange = if_else(((red_count + orange_count) == all_count) & (red_count > 0) & (all_count > 0), TRUE, FALSE),  
+    red_and_orange = if_else(studentgroup == "ALL",
+                             if_else(((red_count + orange_count) == all_count) & (red_count > 0) & (all_count > 0), TRUE, FALSE), 
+                             if_else(((red_count + orange_count) == all_count) & (red_count > 0) & (all_count > 1), TRUE, FALSE)
+    ),
     lowest5_dash = if_else(all_red | all_red_but_1 | majority_red | red_and_orange, TRUE, FALSE)
-  ) 
+    ) %>%
+  arrange(cds, studentgroup, year)
 
 # Combine with original file
-all_2018_wide <- all2018 %>%
+all_wide <- all_years %>%
   mutate(
     color = case_when(
       color == 0 ~ "None",
@@ -150,22 +253,82 @@ all_2018_wide <- all2018 %>%
     )
   ) %>%
   spread(key = indicator, value = color, fill = "None") %>%
-  left_join(all2018_wide_test) %>%
+  left_join(all_wide_test) 
+
+# Create school level data frame
+
+school_file <- all_wide %>%
+  filter(year == "2018",
+         studentgroup == "ALL") %>%
   left_join(gradrate %>%
               select(-indicator)) %>%
   mutate(
     csi_grad = (grad_average < 67),
   )
+  
+# Calculate TSI/ATSI qualification
+group_file <- all_wide %>%
+  filter(studentgroup != "ALL") 
+
+group_file_summary <- group_file %>%
+  select(cds, studentgroup, year, lowest5_dash) %>%
+  spread(key=year, value=lowest5_dash) %>%
+  rename(met_csi_2017 = `2017`,
+         met_csi_2018 = `2018`) %>%
+  mutate(group_met_atsi = met_csi_2017 & met_csi_2018) %>%
+  group_by(cds) %>%
+  summarize(atsi_group_count = sum(group_met_atsi ,na.rm = TRUE)) %>%
+  mutate(met_atsi = atsi_group_count > 0)
+  
+# Import state data
+state_elig <- read_csv('data/sch_eligibility_state_sg.csv') %>%
+  select(cds = CDS,
+         TitleI1718,
+         CDEStatus = AssistanceStatus)
 
 # School category
-support_level <- all_2018_wide %>% 
+support_level <- school_file %>% 
   full_join(state_elig) %>%
-  filter(studentgroup == "ALL") %>%
+  full_join(group_file_summary) %>%
   mutate(identification = case_when(
-    csi_grad == TRUE ~ "CSI-Grad Rate",
-    lowest5_dash == TRUE & TitleI1718 == "Yes" ~ "CSI-Low Perform",
+    csi_grad == TRUE ~ "CSI Grad",
+    lowest5_dash == TRUE & TitleI1718 == "Yes" ~ "CSI Low Perform",
+    met_atsi == TRUE ~ "ATSI", 
     TRUE ~ "General Assistance"
   ))
 
+# Merge in demographic data
+demos <- read_tsv('data/2018/censusenrollratesdownload2018.txt') %>%
+  filter(rtype == "S")
+
+demos_groups <- demos %>%
+  filter(
+    studentgroup %in% c("SED", "SWD", "EL", "FOS")
+  ) %>%
+  select(cds, studentgroup, rate) %>%
+  spread(key = studentgroup, value = rate)
+
+demos_all <- demos %>%
+  select(
+    cds, schoolname, districtname, countyname, totalenrollment
+  ) %>%
+  unique() %>%
+  left_join(demos_groups)
+
+support_level_w_demos <-
+  support_level %>%
+  left_join(demos_all) %>%
+  mutate(school_w_district = paste0(schoolname, ' (', districtname, ')'))
+
+# Audit
+support_audit <- support_level %>%
+  filter(identification != CDEStatus)
+
+# Check for RCOE Data
 rcoe_csi <- support_level %>%
   filter(substr(cds,1,2) == "33")
+
+# Save data for Shiny app
+
+saveRDS(support_level_w_demos, file = 'CSI-ATSI/support_level.RDS')
+saveRDS(group_file, file = 'CSI-ATSI/group_file.RDS')
